7098f4361658f6a0621e4e3713a14e44579d2607
MSI\2220412 <z.ryu.d.0822@gmail.com>
2024-12-13 19:27:38 +0900

    UI出ない。

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,14 @@ DistortionAudioProcessorEditor::DistortionAudioProcessorEditor (DistortionAudioP
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    /*~~~~ここから追加~~~~*/
+    addAndMakeVisible(webComponent);
+    //webComponent.goToURL("http://localhost:5173/");
+    webComponent.goToURL(juce::WebBrowserComponent::getResourceProviderRoot());
+    /*~~~~~追加終わり~~~~~*/
+    
+    setSize(170, 650); // 数値の変更
+    //setSize (400, 300);
 }
 
 DistortionAudioProcessorEditor::~DistortionAudioProcessorEditor()
@@ -27,14 +34,67 @@ void DistortionAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+   
 }
 
 void DistortionAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+     /*~~~~ここから追加~~~~*/
+    webComponent.setBounds(getLocalBounds());
+    /*~~~~~追加終わり~~~~~*/
 }
+
+/*~~~~ここから追加~~~~*/
+std::optional<juce::WebBrowserComponent::Resource>
+DistortionAudioProcessorEditor::getResource(const juce::String& url) {
+    const auto urlToRetrive = url == "/"
+        ? juce::String{ "index.html" }
+    : url.fromFirstOccurrenceOf("/", false, false);
+
+    static auto streamZip = juce::MemoryInputStream(
+        juce::MemoryBlock(BinaryData::assets_zip, BinaryData::assets_zipSize),
+        true);
+
+    static juce::ZipFile archive{ streamZip };
+
+    if (auto* entry = archive.getEntry(urlToRetrive)) {
+        auto entryStream = rawToUniquePtr(archive.createStreamForEntry(*entry));
+        std::vector<std::byte> result((size_t)entryStream->getTotalLength());
+        entryStream->setPosition(0);
+        entryStream->read(result.data(), result.size());
+
+        auto mime = getMimeForExtension(
+            entry->filename.fromLastOccurrenceOf(".", false, false).toLowerCase());
+        return juce::WebBrowserComponent::Resource{ std::move(result),
+                                                   std::move(mime) };
+    }
+    return std::nullopt;
+}
+
+const char* DistortionAudioProcessorEditor::getMimeForExtension(
+    const juce::String& extension) {
+    static const std::unordered_map<juce::String, const char*> mimeMap = {
+        {{"htm"}, "text/html"},
+        {{"html"}, "text/html"},
+        {{"txt"}, "text/plain"},
+        {{"jpg"}, "image/jpeg"},
+        {{"jpeg"}, "image/jpeg"},
+        {{"svg"}, "image/svg+xml"},
+        {{"ico"}, "image/vnd.microsoft.icon"},
+        {{"json"}, "application/json"},
+        {{"png"}, "image/png"},
+        {{"css"}, "text/css"},
+        {{"map"}, "application/json"},
+        {{"js"}, "text/javascript"},
+        {{"woff2"}, "font/woff2"} };
+
+    if (const auto it = mimeMap.find(extension.toLowerCase());
+        it != mimeMap.end())
+        return it->second;
+
+    jassertfalse;
+    return "";
+}
+/*~~~~~追加終わり~~~~~*/
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,6 +11,18 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+/*~~~~ここから追加~~~~*/
+struct SinglePageBrowser : juce::WebBrowserComponent {
+    using WebBrowserComponent::WebBrowserComponent;
+
+    // Prevent page loads from navigating away from our single page web app
+    bool pageAboutToLoad(const juce::String& newURL) override {
+        return newURL == juce::String("http://localhost:5173/") ||
+            newURL == getResourceProviderRoot();
+    }
+};
+/*~~~~~追加終わり~~~~~*/
+
 //==============================================================================
 /**
 */
@@ -23,11 +35,61 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    //==============================================================================
+     /*~~~~ここから追加~~~~*/
+    int getControlParameterIndex(juce::Component&) override {
+        return controlParameterIndexReceiver.getControlParameterIndex();
+    }
+    /*~~~~~追加終わり~~~~~*/
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     DistortionAudioProcessor& audioProcessor;
+
+    /*~~~~ここから追加~~~~*/
+    juce::WebControlParameterIndexReceiver controlParameterIndexReceiver;
+    
+    juce::WebSliderRelay gainRelay{ "gain" };
+    juce::WebSliderRelay panRelay{ "panAngle" };
+    juce::WebComboBoxRelay panRuleRelay{ "panRule" };
+    juce::WebToggleButtonRelay bypassRelay{ "bypass" };
+    
+    //==============================================================================
+    SinglePageBrowser webComponent{
+     juce::WebBrowserComponent::Options{}
+         .withBackend(juce::WebBrowserComponent::Options::Backend::webview2)
+         .withWinWebView2Options(
+             juce::WebBrowserComponent::Options::WinWebView2{}
+                 .withUserDataFolder(juce::File::getSpecialLocation(
+                     juce::File::SpecialLocationType::tempDirectory)))
+         .withOptionsFrom(gainRelay)
+         .withOptionsFrom(panRelay)
+         .withOptionsFrom(panRuleRelay)
+         .withOptionsFrom(bypassRelay)
+         .withOptionsFrom(controlParameterIndexReceiver)
+         .withResourceProvider(
+             [this](const auto& url) { return getResource(url); },
+             juce::URL{"http://localhost:5173/"}.getOrigin()) };
+
+    std::optional<juce::WebBrowserComponent::Resource> getResource(
+        const juce::String& url);
+
+    const char* getMimeForExtension(const juce::String& extension);
+    /*~~~~~追加終わり~~~~~*/
+    //==============================================================================
+
+  /*~~~~ここから追加~~~~*/
+    juce::WebSliderParameterAttachment gainAttachment{
+        *audioProcessor.parameters.getParameter("gain"), gainRelay, nullptr };
+    juce::WebSliderParameterAttachment panAttachment{
+        *audioProcessor.parameters.getParameter("panAngle"), panRelay, nullptr };
+    juce::WebComboBoxParameterAttachment panModeAttachment{
+        *audioProcessor.parameters.getParameter("panRule"), panRuleRelay,
+        nullptr };
+    juce::WebToggleButtonParameterAttachment bypassAttachment{
+        *audioProcessor.parameters.getParameter("bypass"), bypassRelay, nullptr };
+    /*~~~~~追加終わり~~~~~*/
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DistortionAudioProcessorEditor)
 };
